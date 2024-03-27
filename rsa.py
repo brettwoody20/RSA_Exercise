d3e0c5a2481b001b8b729a2f26aab9343a66458e
Brett Woodcock <brettwoody20@gmail.com>
2024-03-26 22:35:38 -0500

    outllined the functionality of the RSA class in comments

--- rsa.py
+++ rsa.py
@@ -1,1 +1,14 @@
 #Could implement RSA class here
+
+    #static list of primes
+    #e is considered static
+    
+    #constructor generates p, q, N (private variables)
+    
+    #function to get public key N, e
+    
+    #function to encrypt a message using RSA (int m, string plaintext) -> (int c, string encrypted)
+    
+    #function to decrypt a message using RSA (int c, string encrypted) -> (string plaintext)
+    
+#NOTE: if RSA would be a class stored on a server, then they would not store the p, q values- those would be stored on the client's side 
